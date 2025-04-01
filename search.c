4a103157e5a10497ddc9d6c7e1c22f12d85ab6ad
Fredrick Bouloute <89982743+bouloutef04@users.noreply.github.com>
2025-04-01 16:23:06 -0400

    Added waiting for threads and print statements.

--- search.c
+++ search.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // Reader-writer lock for synchronizing access to the hash table
 extern pthread_rwlock_t rwlock;
@@ -13,13 +14,27 @@ extern hashRecord *head;
 extern int lock_acquisitions;
 extern int lock_releases;
 
+// Mutex and condition variable for coordinating thread execution 
+extern pthread_mutex_t cv_mutex;
+extern pthread_cond_t cv_insert_done;
+
 hashRecord* search(const char* key) {
   //Create hash value
   uint32_t hash = jenkins_one_at_a_time_hash(key);
+  int waiting = 0;
+  time_t now = time(NULL);
   //If lock not acquired, print timestamp and what is waiting for
-  if(pthread_rwlock_rdlock(rwlock) != 0){
-  
+  while(pthread_rwlock_rdlock(rwlock) != 0){
+    waiting = 1;
+    //print timestamp & wait
+    printf("%ld: WAITING ON INSERTS\n", now);
+    pthread_cond_wait(&cv_insert_done, cv_mutex);
   }
+  //If thread has ever waited, state that it is now awakened
+  if(waiting == 1){
+    printf("%ld: SEARCH AWAKENED\n", now);
+  }
+  printf("%ld: READ LOCK ACQUIRED", now);
   lock_acquisitions++;
   
   //Search through hash table 
@@ -34,6 +49,7 @@ hashRecord* search(const char* key) {
   }
 
   //Unlock read lock
+  printf("%ld: READ LOCK RELEASED", now);
   pthread_rwlock_unlock(rwlock);
   lock_releases++;
   return NULL;
