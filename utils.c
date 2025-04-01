6870aadcfc6fd2a4c2d1a8861e6454f753d4d660
unknown <mbndanh2k4@gmail.com>
2025-04-01 16:59:59 -0400

    insert updates

--- utils.c
+++ utils.c
@@ -1,4 +1,21 @@
 #include "chash.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+// Global file pointer for output (make sure to open it in your main function)
+extern FILE *output_file;
+
+void log_event(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    // Write to the output file
+    vfprintf(output_file, format, args);
+    fprintf(output_file, "\n");
+    
+    va_end(args);
+}
 
 uint32_t jenkins_one_at_a_time_hash(const char *key) {
     uint32_t hash = 0;
@@ -12,3 +29,4 @@ uint32_t jenkins_one_at_a_time_hash(const char *key) {
     hash += (hash << 15);
     return hash;
 }
+
