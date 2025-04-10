70b325f737a2a2ae24b74dad337dc33d52cff439
unknown <mbndanh2k4@gmail.com>
2025-04-10 15:53:46 -0400

    adjusted prints in insert

--- insert.c
+++ insert.c
@@ -9,7 +9,7 @@ void insert_record(const char *name, uint32_t salary) {
     uint32_t hash_val = jenkins_one_at_a_time_hash(name);
 
     // Log before acquiring the write lock
-    log_event("%ld,WRITE LOCK ACQUIRED", time(NULL));
+    log_event("%ld,WRITE LOCK ACQUIRED\n", time(NULL));
 
     // Acquire the write lock to modify the hash table
     pthread_rwlock_wrlock(&rwlock);
@@ -23,10 +23,10 @@ void insert_record(const char *name, uint32_t salary) {
         if (curr->hash == hash_val && strcmp(curr->name, name) == 0) {
             // If found, update the salary
             curr->salary = salary;
-            log_event("%ld,INSERT,%s,%u", time(NULL), name, salary);
+            log_event("%ld,INSERT,%s,%u\n", time(NULL), name, salary);
             pthread_rwlock_unlock(&rwlock);
             lock_releases++;
-            log_event("%ld,WRITE LOCK RELEASED", time(NULL));
+            log_event("%ld,WRITE LOCK RELEASED\n", time(NULL));
             return;
         }
 
@@ -34,7 +34,7 @@ void insert_record(const char *name, uint32_t salary) {
         curr = curr->next;
     }
 
-    // Record not found; create a new node and allocate space
+    // Create a new node and allocate space
     hashRecord *newRecord = (hashRecord *)malloc(sizeof(hashRecord));
     
     // Space wasn't properly allocated
@@ -56,10 +56,10 @@ void insert_record(const char *name, uint32_t salary) {
     head = newRecord;
 
     // Log the insert command
-    log_event("%ld,INSERT,%s,%u", time(NULL), name, salary);
+    log_event("%ld,INSERT,%s,%u\n", time(NULL), name, salary);
 
     // Release the write lock
     pthread_rwlock_unlock(&rwlock);
     lock_releases++;
-    log_event("%ld,WRITE LOCK RELEASED", time(NULL));
+    log_event("%ld,WRITE LOCK RELEASED\n", time(NULL));
 }
