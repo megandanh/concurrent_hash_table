dce93a88add342b12445d92f4a1d907298f0f7d7
unknown <mbndanh2k4@gmail.com>
2025-04-10 23:17:06 -0400

    debug insert

--- utils.c
+++ utils.c
@@ -8,14 +8,15 @@ extern FILE *output_file;
 
 void log_event(const char *format, ...) {
     va_list args;
-    va_start(args, format);
-
-    // Write to the output file
-    vfprintf(output_file, format, args);
-    fprintf(output_file, "\n");
-    
-    va_end(args);
+     va_start(args, format);
+ 
+     // Write to the output file
+     vfprintf(output_file, format, args);
+     fprintf(output_file, "\n");
+     
+     va_end(args);
 }
+
 
 uint32_t jenkins_one_at_a_time_hash(const char *key) {
     uint32_t hash = 0;
